bff9453c6a33ea424a218ce2ef00ed9c5048e359
kenttv11 <kenttv11@yandex.ru>
2025-05-12 19:26:54 +0300

    Fix 3.4 work on test

--- backend/api_routers/support/auth.py
+++ backend/api_routers/support/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from backend.database.db import Support
+from backend.database.utils import get_db_session
+from backend.database.db import User
 from backend.config.crypto import verify_password
 from backend.config.logger import get_logger
 from backend.shemas_enums import support_schemas
@@ -8,19 +9,12 @@ from backend.shemas_enums import support_schemas
 router = APIRouter()
 logger = get_logger("support_auth")
 
-def get_db():
-    from backend.database.db import SessionLocal
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 @router.post("/auth/login")
-def login_support(data: support_schemas.SupportLogin, db: Session = Depends(get_db)):
-    user = db.query(Support).filter_by(email=data.email).first()
-    if not user or not verify_password(data.password, user.password_hash):
+def login_support(data: support_schemas.SupportLogin, db: Session = Depends(get_db_session)):
+    user = db.query(User).filter_by(email=data.email).one_or_none()
+    if not user or not user.support_profile or not verify_password(data.password, user.password_hash):
         logger.warning(f"Неудачная попытка входа саппорта: {data.email}")
         raise HTTPException(status_code=401, detail="Неверные учетные данные")
+    support_profile = user.support_profile
     logger.info(f"Саппорт вошел: {user.email}")
-    return {"id": user.id, "email": user.email} 
+    return {"id": support_profile.id, "email": user.email} 
